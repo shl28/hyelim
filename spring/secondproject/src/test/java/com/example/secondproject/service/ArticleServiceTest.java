7da3c3751cf99a601497d548dc9a8bb36ece7651
shl28 <kklim1218@gmail.com>
2026-04-27 18:11:18 +0900

    a0427_2

--- spring/secondproject/src/test/java/com/example/secondproject/service/ArticleServiceTest.java
+++ spring/secondproject/src/test/java/com/example/secondproject/service/ArticleServiceTest.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,6 +16,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 
 class ArticleServiceTest {
     @Autowired
